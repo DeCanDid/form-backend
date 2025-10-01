7d0e36698b16e38138c6decda4449b5587930ef0
candid <olacandid@gmail.com>
2025-10-01 17:30:22 +0100

    'body-parser-removed'

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+
+// for railway port
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 // Logging middleware
 app.use((req, res, next) => {
