ffc269bd1ce8025b38678c7880220f2911802cf0
candid <olacandid@gmail.com>
2025-10-01 22:12:48 +0100

    'update.server'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // const PORT = 5000;
 
 // for railway port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // app.use(
 //   cors({
@@ -23,6 +23,7 @@ app.use(
     origin: "*",
     methods: ["POST", "GET", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
+    credentials: true,
   })
 );
 
